b506cf9085cbdaef7dc3c9dca431b31cf3988972
Node <8974108+qwenode@users.noreply.github.com>
2023-03-26 00:02:56 +0800

    11

--- duplicate.go
+++ duplicate.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/qwenode/gogo/convert"
-	"github.com/qwenode/gogo/file"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
+
+	"github.com/qwenode/color"
+	"github.com/qwenode/gogo/convert"
+	"github.com/qwenode/gogo/file"
 )
 
 func main() {
-	source := flag.String("s", "", "set source directory")
-	to := flag.String("t", "", "set backup directory")
-	flag.Parse()
-	sourceDir, _ := filepath.Abs(*source)
-	toDir, _ := filepath.Abs(*to)
+	run()
+	color.SuccessMessage("全部检查完毕,10分钟后关闭窗口,您也可以现在关闭")
+	time.Sleep(time.Second * 600)
+}
+func run() {
+	// catch all panic
+	defer func() {
+		err := recover()
+		if err != nil {
+			//debug.PrintStack()
+			log.Println("System Error:", err)
+		}
+	}()
+	sourceDir, _ := filepath.Abs("./")
+	toDir := filepath.Join(sourceDir, "_dup")
 	log.Println(sourceDir, toDir)
 	if !file.Exist(sourceDir) || !file.IsDirectory(sourceDir) {
 		log.Fatalln("source directory does not exist")
 	}
 	if !file.Exist(toDir) || !file.IsDirectory(toDir) {
+		os.MkdirAll(toDir, os.ModePerm)
 		log.Fatalln("backup directory does not exist")
 	}
 	if sourceDir == toDir {
@@ -31,15 +45,20 @@ func main() {
 	moved := 0
 	skipped := 0
 	failed := 0
-	_ = filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+	_ = filepath.WalkDir(sourceDir, func(path string, info fs.DirEntry, err error) error {
+		if strings.HasPrefix(path, toDir) {
+			return nil
+		}
+		color.InfoMessage("检查:%s", path)
 		if err != nil || info.IsDir() {
+			color.InfoMessage("文件夹,跳过")
 			return nil
 		}
-		if info.Size() < 10 {
-			skipped++
-			log.Println("skipped for small size:", path)
-			return nil
-		}
+		//if info.Size() < 10 {
+		//	skipped++
+		//	log.Println("skipped for small size:", path)
+		//	return nil
+		//}
 		//log.Println(path, info.IsDir(), info.Name(), err)
 		sha, err := file.Sha1(path)
 		if err != nil {
@@ -48,23 +67,24 @@ func main() {
 		if _, ok := hashList[sha]; !ok {
 			hashList[sha] = path
 			skipped++
-			log.Println("Skipped:", path)
+			color.InfoMessage("文件未重复:%s", path)
 			return nil
 		}
-		toFile := toDir + "/" + info.Name()
+		toFile := filepath.Join(toDir, info.Name())
 		if file.Exist(toFile) {
 			toFile = fmt.Sprintf("%s/%s_%s", toDir, convert.ToString(time.Now().Unix()), info.Name())
 		}
-		log.Println("Moving File:", path, " To:", toFile)
+		color.WarningMessage("重复文件=>%s", toFile)
 		err = os.Rename(path, toFile)
 		if err != nil {
 			failed++
-			log.Println("Move failed.")
+			color.ErrorMessage("重复文件移动失败:%s", err.Error())
 			return nil
 		}
 		moved++
-		log.Println("Success Moved.")
+		color.SuccessMessage("文件移动成功:%s", path)
 		return nil
 	})
-	log.Println(fmt.Sprintf("Skipped:%d,Moved:%d,Failed:%d", skipped, moved, failed))
+	color.SuccessMessage(fmt.Sprintf("数据统计=>跳过:%d,重复:%d,失败:%d", skipped, moved, failed))
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module duplicate
 
 go 1.15
 
-require github.com/qwenode/gogo v1.28.0
+require (
+	github.com/qwenode/color v1.14.0 // indirect
+	github.com/qwenode/gogo v1.28.0
+)
